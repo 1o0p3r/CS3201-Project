e72a723e3e81a90b42989e8cfced61291d813a88
DESKTOP-I8I0R59\limweijie <limweijie1994@gmail.com>
2017-09-04 22:30:42 +0800

    Made some changes i.e. new methods are added, To-dos : Finish the addition of synonyoms to table, Finish the parsing of Select Clause

--- QueryPreprocessor/QueryPreprocessor/ParserQuery.h
+++ QueryPreprocessor/QueryPreprocessor/ParserQuery.h
@@ -9,16 +9,24 @@ using std::vector;
 class ParserQuery
 {
 private:
-	vector<string> validEntities;
 	int numClauses;
+	vector<vector<string>> synonymList;
+	vector<string> validEntities;
 	string declarationString;
 	string queryString;
-	void parseQueryStatement(string);
-	void parseEntityAndSynonym(string);
 
+	void parseQueryStatement(string);
 	void parseLine(string, string);
+	bool checkEntityAndSynonym(string, string);
+	bool isValidEntity(string);
+	bool InEntityList(string entity);
+	bool inEntityList(string);
 	string parseEntityOrQuery(string, int);
+	string parseEntity(string , string, int);
+	bool parseDeclaration(vector<string> splitString);
 	string appendAdditionalSpace(string, string);
+	vector<string> split(string, char);
+	void setSynonymList(vector<vector<string>> synonymList);
 public:
 	ParserQuery();
 	~ParserQuery();
