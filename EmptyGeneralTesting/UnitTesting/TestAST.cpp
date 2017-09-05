f589a1cb3b49ee03cce99bf651fbdfce4f6c6e65
Niveetha <niveetha.nair@gmail.com>
2017-09-05 13:53:17 +0800

    EmptyGeneralTesting Files

---
+++ EmptyGeneralTesting/UnitTesting/TestAST.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "TNode.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{		
+	TEST_CLASS(TestAST)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			TNode T;
+			// TODO: Your test code here
+		}
+
+	};
+}
---
+++ EmptyGeneralTesting/source/Parser.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "PKB.h"
+#include "TNode.h"
+#include "Parser.h"
+
+int Parse () {
+
+	return 0;
+}
