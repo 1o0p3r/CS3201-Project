81c5c84d1b4946b7147731f95f6c544931d93425
Lim Jie <limjie1994@gmail.com>
2017-09-06 02:17:25 +0800

    Implemented PKB, added some dependencies and removed some files

--- EmptyGeneralTesting/source/PKB.h
+++ EmptyGeneralTesting/source/PKB.h
@@ -8,14 +8,9 @@
 using namespace std;
 typedef short PROC;
 
-class TNode;
-
 class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
 public:
-	static VarTable* varTable; 
-	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
-
+	PKB();
 };
