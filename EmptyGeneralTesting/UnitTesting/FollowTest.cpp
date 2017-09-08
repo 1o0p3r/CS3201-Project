2799b37c69dd4998de3b4f302eea5964983978ea
Lim Jie <limjie1994@gmail.com>
2017-09-08 20:14:04 +0800

    Added more testcases

--- EmptyGeneralTesting/UnitTesting/FollowTest.cpp
+++ EmptyGeneralTesting/UnitTesting/FollowTest.cpp
@@ -16,6 +16,18 @@ namespace UnitTesting
 			follow.setFollows(1, 2);
 			Assert::AreEqual(2, follow.getFollows(1)[0]);
 			Assert::AreEqual(1, follow.getFollowedBy(2)[0]);
+
+			follow.setFollows(2, 3);
+			vector<int> actual;
+			actual.push_back(2);
+			actual.push_back(3);
+			Assert::AreEqual(actual[0], follow.getFollowsStar(1)[0]);
+			Assert::AreEqual(actual[1], follow.getFollowsStar(1)[1]);
+			actual.clear();
+			actual.push_back(2);
+			actual.push_back(1);
+			Assert::AreEqual(actual[0], follow.getFollowedByStar(3)[0]);
+			Assert::AreEqual(actual[1], follow.getFollowedByStar(3)[1]);
 		}
 
 	};
