3b6b3af2c89b25f961c0b8837aa72e57e5dd2925
Lim Jie <limjie1994@gmail.com>
2017-09-08 20:13:18 +0800

    Added documentation and made it such that setFollows calls all other setters

--- EmptyGeneralTesting/SPA/Follow.cpp
+++ EmptyGeneralTesting/SPA/Follow.cpp
@@ -19,11 +19,13 @@ void Follow::setFollows(int s1, int s2) {
 	followsTable[s1] = s2;
 	setFollowedBy(s2, s1);
 	setFollowsStar(s1, s2);
+	int follower = getFollowedBy(s1)[0];
+	while (follower != 0) {
+		setFollowsStar(follower, s2);
+		follower = getFollowedBy(follower)[0];
+	}
 }
 
-/*
-	Sets s2 to be followed by s1. Meaning: s1 > s2
-*/
 void Follow::setFollowedBy(int s1, int s2) {
 	followedByTable.resize(s1 + 1);
 	followedByTable[s1] = s2;
@@ -33,11 +35,13 @@ void Follow::setFollowsStar(int s1, int s2) {
 	if (s1 <= 0 || s2 <= 0) {
 		throw new invalid_argument("Statement Number must be more than 0.");
 	}
+	followsStarTable.resize(s1 + 1);
 	followsStarTable[s1].push_back(s2);
 	setFollowedByStar(s2, s1);
 }
 
 void Follow::setFollowedByStar(int s1, int s2) {
+	followedByStarTable.resize(s1 + 1);
 	followedByStarTable[s1].push_back(s2);
 }
 
--- EmptyGeneralTesting/SPA/Follow.h
+++ EmptyGeneralTesting/SPA/Follow.h
@@ -14,11 +14,34 @@ public:
 	@returns void
 	*/
 	void setFollows(int s1, int s2);
-	void setFollowsStar(int s1, int s2);
 
+	/**
+	Gets the statment number that follows the input statement.
+
+	@param statementNum the statement number of the input statement
+	@returns A vector containing one int value, the statement number which follows the input statement
+	*/
 	vector<int> getFollows(int statementNum);
+	/**
+	Gets the statment number that is followed by the input statement.
+
+	@param statementNum the statement number of the input statement
+	@returns A vector containing one int value, the statement number which is followed by the input statement
+	*/
 	vector<int> getFollowedBy(int statementNum);
+	/**
+	Gets the statment numbers that follows* the input statement.
+
+	@param statementNum the statement number of the input statement
+	@returns A vector containing integers, the statement numbers which follows* the input statement
+	*/
 	vector<int> getFollowsStar(int statementNum);
+	/**
+	Gets the statment numbers that is followed* by the input statement.
+
+	@param statementNum the statement number of the input statement
+	@returns A vector containing integers, the statement numbers is followed* by the input statement
+	*/
 	vector<int> getFollowedByStar(int statementNum);
 
 private:
@@ -28,5 +51,6 @@ private:
 	vector<vector<int>> followedByStarTable;
 
 	void setFollowedBy(int s1, int s2);
+	void setFollowsStar(int s1, int s2);
 	void setFollowedByStar(int s1, int s2);
 };
