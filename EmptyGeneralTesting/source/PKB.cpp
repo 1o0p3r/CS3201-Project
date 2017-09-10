6a02e75845bece642a11d07dacda047dcf9a912e
n <niveetha.nair@gmail.com>
2017-09-10 18:14:56 +0800

    var and proc indexing in PKB

--- EmptyGeneralTesting/source/PKB.cpp
+++ EmptyGeneralTesting/source/PKB.cpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "PKB.h"
+#include "Follow.h"
+#include "Parent.h"
+#include "Modify.h"
+#include "Use.h"
+
 #include<stdio.h>
 #include <iostream>
 #include <string>
@@ -7,19 +13,53 @@
 
 using namespace std;
 
-#include "PKB.h"
-#include "Follow.h"
-#include "Parent.h"
-/*
-#include "Modify.h"
-#include "Use.h"
-*/
-
 PKB::PKB() {
+	
 	Follow follow;
 	Parent parent;
-	/*
 	Modify modify;
 	Use use;
-	*/
+
+	vector<string> varIndexTable;
+	vector<string> procIndexTable;
+}
+
+void PKB::setVarIndex(string varName) {
+
+	if (find(varIndexTable.begin(), varIndexTable.end(), varName) != varIndexTable.end())
+		return;
+	
+	else 
+		varIndexTable.push_back(varName);
+}
+
+int PKB::getVarIndex(string varName) {
+
+	int pos = find(varIndexTable.begin(), varIndexTable.end(), varName) - varIndexTable.begin();
+
+	if (pos < varIndexTable.size())
+		return pos;
+
+	else
+		throw new invalid_argument("Variable not found in PKB.");
+}
+
+void PKB::setProcIndex(string varName) {
+
+	if (find(procIndexTable.begin(), procIndexTable.end(), varName) != procIndexTable.end())
+		return;
+
+	else
+		procIndexTable.push_back(varName);
+}
+
+int PKB::getProcIndex(string varName) {
+
+	int pos = find(procIndexTable.begin(), procIndexTable.end(), varName) - procIndexTable.begin();
+
+	if (pos < procIndexTable.size())
+		return pos;
+
+	else
+		throw new invalid_argument("Variable not found in PKB.");
 }
--- EmptyGeneralTesting/source/PKB.h
+++ EmptyGeneralTesting/source/PKB.h
@@ -11,6 +11,18 @@ typedef short PROC;
 class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
+
 public:
+
 	PKB();
+
+	void PKB::setVarIndex(string varName);
+	int PKB::getVarIndex(string varName);
+	void PKB::setProcIndex(string varName);
+	int PKB::getProcIndex(string varName);
+
+private:
+
+	vector<string> varIndexTable;
+	vector<string> procIndexTable;
 };
