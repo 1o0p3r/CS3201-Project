1f740cabb0a6e66c5cfc3940e891049f2bd9ca43
n <niveetha.nair@gmail.com>
2017-09-12 11:17:40 +0800

    commenting on functions in PKB

--- EmptyGeneralTesting/source/PKB.h
+++ EmptyGeneralTesting/source/PKB.h
@@ -31,6 +31,7 @@ public:
 	@returns A vector containing one int value, the statement number which follows the input statement
 	*/
 	vector<int> getFollows(int statementNum);
+	
 	/**
 	Gets the statment number that is followed by the input statement.
 
@@ -38,6 +39,7 @@ public:
 	@returns A vector containing one int value, the statement number which is followed by the input statement
 	*/
 	vector<int> getFollowedBy(int statementNum);
+	
 	/**
 	Gets the statment numbers that follows* the input statement.
 
@@ -45,6 +47,7 @@ public:
 	@returns A vector containing integers, the statement numbers which follows* the input statement
 	*/
 	vector<int> getFollowsStar(int statementNum);
+	
 	/**
 	Gets the statment numbers that is followed* by the input statement.
 
@@ -69,6 +72,7 @@ public:
 	@returns A vector containing one int value, the statement number which is the parent of the input statement
 	*/
 	vector<int> getParent(int statementNum);
+	
 	/**
 	Gets the statment number that is the child of the input statement.
 
@@ -76,6 +80,7 @@ public:
 	@returns A vector containing one int value, the statement number which is the child of the input statement
 	*/
 	vector<int> getChild(int statementNum);
+	
 	/**
 	Gets the statment numbers that are the parent* of the input statement.
 
@@ -83,6 +88,7 @@ public:
 	@returns A vector containing integers, the statement numbers which are the parent* of the input statement
 	*/
 	vector<int> getParentStar(int statementNum);
+	
 	/**
 	Gets the statment numbers that are the child* of the input statement.
 
@@ -91,20 +97,106 @@ public:
 	*/
 	vector<int> getChildStar(int statementNum);
 
+	/**
+	Sets s to modifies varName. Implicitly set varName to be modifiedBy s.
+
+	@param s statement number of the input statement
+	@param s2 variable the statement modifies
+	@returns void
+	*/
 	void setModifies(int s, string varName);
+
+	/**
+	Sets proc to modifies varName. Implicitly set varName to be modifiedBy proc.
+
+	@param procName procedure the statement line is nested in
+	@param varName variable the procedure modifies
+	@returns void
+	*/
 	void setProcModifies(string procName, string varName);
 
+	/**
+	Gets the variables statement s modifies
+
+	@param s statement number of the input statement 
+	@returns a vector containing strings the names of the variables the input statement modifies
+	*/
 	vector<string> getModifies(int s);
+
+	/**
+	Gets the statements modified by variable varName
+
+	@param varName variable being modified by input statements
+	@returns a vector containing integers of statement numbers that modified the variable varName
+	*/
 	vector<int> getModifiedBy(string varName);
+
+	/**
+	Gets the variables the procedure modifies
+
+	@param procName procedure name of the input procedure 
+	@returns a vector containing strings the names of the variables the input statement modifies
+	*/
 	vector<string> getProcModifies(string procName);
+
+	/**
+	Gets the procedures modified by variable varName
+
+	@param varName variable being modified by input procedures
+	@returns a vector containing strings of procedure names that modified the variable varName
+	*/
 	vector<string> getProcModifiedBy(string varName);
 
+
+	/**
+	Sets s to uses varName. Implicitly set varName to be UsedBy s.
+
+	@param s statement number of the input statement
+	@param s2 variable the statement uses
+	@returns void
+	*/
 	void setUses(int s, string varName);
+
+	/**
+	Sets proc to uses varName. Implicitly set varName to be UsedBy proc.
+
+	@param procName procedure the variable is nested in
+	@param varName variable the procedure uses
+	@returns void
+	*/
 	void setProcUses(string procName, string varName);
 
+
+	/**
+	Gets the variables statement s uses
+
+	@param s statement number of the input statement
+	@returns a vector containing strings the names of the variables the input statement uses
+	*/
 	vector<string> getUses(int s);
+
+	/**
+	Gets the statements used by variable varName
+
+	@param varName variable being used by input statements
+	@returns a vector containing integers of statement numbers that used the variable varName
+	*/
 	vector<int> getUsedBy(string varName);
+
+	/**
+	Gets the variables the procedure uses
+
+	@param procName procedure name of the input procedure
+	@returns a vector containing strings the names of the variables the input statement uses
+	*/
 	vector<string> getProcUses(string procName);
+
+	/**
+	Gets the procedures used by variable varName
+
+	@param varName variable being used by input procedures
+	@returns a vector containing strings of procedure names that used the variable varName
+	*/
 	vector<string> getProcUsedBy(string varName);
 
 
