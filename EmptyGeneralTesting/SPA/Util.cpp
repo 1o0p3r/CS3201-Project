18cb9b04463987f141c5199b43dfb1c26cde9ae7
Lim Jie <limjie1994@gmail.com>
2017-09-14 12:29:40 +0800

    Added a Util class and implemented includeBrackets

---
+++ EmptyGeneralTesting/SPA/Util.cpp
@@ -0,0 +1,91 @@
+#include "Util.h"
+#include <iostream>
+
+string Util::insertBrackets(string results) {
+	//string results = input;
+	int index = 0;
+	while (index < results.size()) {
+		if (results[index] == '*') {
+			if (results[index - 1] != ')') {
+				results.insert(index + 2, ")");
+				results.insert(index - 1, "(");
+			} else {
+				int counter = 1;
+				int back = index - 1;
+				while (counter > 0) {
+					back--;
+					if (results[back] == '(') {
+						counter--;
+					} else if (results[back] == ')') {
+						counter++;
+					}
+				}
+				results.insert(index + 2, ")");
+				results.insert(back, "(");
+			}
+			index++;
+		}
+		index++;
+	}
+	index = 0;
+	while (index < results.size()) {
+		if (results[index] == '+' || results[index] == '-') {
+			if (results[index - 1] != ')' && results[index + 1] != '(') {
+				results.insert(index + 2, ")");
+				results.insert(index - 1, "(");
+			} else if (results[index - 1] == ')' && results[index + 1] != '(') {
+				int counter = 1;
+				int back = index - 2;
+				while (counter > 0) {
+					if (results[back] == '(') {
+						counter--;
+					} else if (results[back] == ')') {
+						counter++;
+					}
+					back--;
+				}
+				results.insert(index + 2, ")");
+				results.insert(back + 1, "(");
+			} else if (results[index + 1] == '(' && results[index - 1] != ')') {
+				int counter = 1;
+				int front = index + 2;
+				while (counter > 0) {
+					if (results[front] == ')') {
+						counter--;
+					} else if (results[front] == '(') {
+						counter++;
+					}
+					front++;
+				}
+				results.insert(front - 1, ")");
+				results.insert(index - 1, "(");
+			} else {
+				int counter = 1;
+				int back = index - 2;
+				int front = index + 2;
+				while (counter > 0) {
+					if (results[back] == '(') {
+						counter--;
+					} else if (results[back] == ')') {
+						counter++;
+					}
+					back--;
+				}
+				counter = 1;
+				while (counter > 0) {
+					if (results[front] == ')') {
+						counter--;
+					} else if (results[front] == '(') {
+						counter++;
+					}
+					front++;
+				}
+				results.insert(front - 1, ")");
+				results.insert(back + 1, "(");
+			}
+			index++;
+		}
+		index++;
+	}
+	return results;
+}
