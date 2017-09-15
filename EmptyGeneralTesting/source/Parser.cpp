54cdc6e10b37eb3426ee545428e5863f590bb420
n <niveetha.nair@gmail.com>
2017-09-15 18:54:30 +0800

    a little work on assign type statement

--- EmptyGeneralTesting/source/Parser.cpp
+++ EmptyGeneralTesting/source/Parser.cpp
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 #include <Util.h>
+#include "PKB.h"
+#include "Parser.h"
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,12 +12,9 @@
 
 using namespace std;
 
-#include "PKB.h"
-#include "Parser.h"
-
-
-
 bool Parse(string fileName) {
+
+	PKB pkb;
 
 	ifstream file(fileName);
 	string line;
@@ -25,30 +25,47 @@ bool Parse(string fileName) {
 
 	vector<string> firstLine = Util::splitLine(line, ' ');
 
-	if (firstLine[0] != "procedure" && firstLine[3] == "{") {
+	if (firstLine[0] != "procedure" && firstLine[2] == "{") {
+		
 		return false;
 	}
 
 	else {
-		procName = firstLine[1];
+		
+		procName = firstLine[1]; //need to validate > does not start with digit
 	}
 
-	while (getline(file, line)) {
+	while (getline(file, line)) { //read line by line
+		
 		lineCounter++;
 		vector<string> nextLine = Util::splitLine(line, ' ');
 		
-		if (nextLine[0] == "}") {
-			if (nextLine[1] == "else") {
-				//more
-			 }
+		if (nextLine[0] == "else") { // line starts with else, no statement number
+			
 			lineCounter--;
+			continue;
 		}
 
 		else {
-			if (isSameLevel)
+			
+			if (nextLine[1] == "=") { //assign statements
+				
+				pkb.setStatementType(lineCounter, 2);
+				isSameLevel = true;
+				
+				if (lineCounter != 1) {			
+					pkb.setFollows(lineCounter - 1, lineCounter);
+				}
 
+				for (int i = 2; i < nextLine.size(); i++) {
+
+					if (isdigit(nextLine[i])) //if value is a digit, add to constants table
+						return false;
+				}
+			} //end of assign statemnt
+			return false;
 		}
 	}
 
-	return true;
+	return false;
 }
