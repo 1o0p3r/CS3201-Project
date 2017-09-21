b6fba5afe43239014ad428f1e40ae4368878aedb
Lim Jie <limjie1994@gmail.com>
2017-09-22 00:42:35 +0800

    Added testcases for parser and frontend

---
+++ Code09/UnitTesting/ParserTest.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Parser.h"
+#include "PKB.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting {
+	TEST_CLASS(ParserTest) {
+	public:
+		TEST_METHOD(parse) {
+			PKB pkb;
+			string filename = "Sample-Source.txt";
+			Assert::IsTrue(Parse(filename, pkb));
+		}
+	};
+}
